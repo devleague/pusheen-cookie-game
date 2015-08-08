cfdd8e55600f232b0d4202ee103daea5af7dad86
Sean Nakamura <seanenakamura@gmail.com>
2015-08-07 16:40:40 -1000

    added start button

--- script.js
+++ script.js
@@ -4,36 +4,46 @@ var myCookies = 0;
 
 var pusheenCookies = 0;
 
+
 window.onload = function (){
+  var cookieCount = document.getElementById('cookies');
+  var pusheenCount = document.getElementById('pusheencookies');
+  var myCount = document.getElementById('mycookies');
 
-  var pusheenEats = window.setInterval(function (){
-    if (cookies > 0){
-      if (pusheenCookies < 100){
-        pusheenCookies += 1;
-        cookies -= 1;
-      }
-    }
-    if (cookies === 0){
-      clearInterval(pusheenEats);
-      if (myCookies > pusheenCookies){
-        alert('You\'ve won!');
-      }else {
-        alert('YOU LOST ALL THE COOKIES!');
-      }
-    }
-    document.getElementById('cookies').innerHTML = cookies;
-    document.getElementById('pusheencookies').innerHTML = pusheenCookies;
-  }, 250);
+  cookieCount.innerHTML = cookies;
+  myCount.innerHTML = myCookies;
+  pusheenCount.innerHTML = pusheenCookies;
 
-  document.getElementById('getcookie').onclick =
-    function getCookie (){
-      if (cookies > 0){
-        cookies -= 1;
-        myCookies += 1;
-      }
-      document.getElementById('cookies').innerHTML = cookies;
-      document.getElementById('mycookies').innerHTML = 'My Cookies: ' + myCookies;
+  document.getElementById('start').onclick =
+    function (){
+
+      var pusheenEats = window.setInterval(function (){
+        if (cookies > 0){
+          if (pusheenCookies < 100){
+            pusheenCookies += 1;
+            cookies -= 1;
+          }
+        }
+        if (cookies === 0){
+          clearInterval(pusheenEats);
+          if (myCookies > pusheenCookies){
+            alert('You\'ve won!');
+          }else {
+            alert('YOU LOST ALL THE COOKIES!');
+          }
+        }
+        cookieCount.innerHTML = cookies;
+        pusheenCount.innerHTML = pusheenCookies;
+      }, 250);
+
+      document.getElementById('getcookie').onclick =
+        function getCookie (){
+          if (cookies > 0){
+            cookies -= 1;
+            myCookies += 1;
+          }
+          cookieCount.innerHTML = cookies;
+          myCount.innerHTML = myCookies;
+        };
     };
-
-
 };
