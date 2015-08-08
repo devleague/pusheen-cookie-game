3053f6b179a57d1c6cd41c1bc60b2db6c723d338
Sean Nakamura <seanenakamura@gmail.com>
2015-08-07 17:43:48 -1000

    added start validation for one click only

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ var myCookies = 0;
 
 var pusheenCookies = 0;
 
+var isActive = false;
+
 
 window.onload = function (){
   var cookieCount = document.getElementById('cookies');
@@ -15,10 +17,15 @@ window.onload = function (){
   myCount.innerHTML = myCookies;
   pusheenCount.innerHTML = pusheenCookies;
 
-  var startGame = function (){
+  function startGame (){
     cookies = 100;
-    myCookies = 0;
     pusheenCookies = 0;
+    myCookies = 0;
+
+    cookieCount.innerHTML = cookies;
+    pusheenCount.innerHTML = pusheenCookies;
+    myCount.innerHTML = myCookies;
+    result.innerHTML = '';
 
     var pusheenEats = window.setInterval(function (){
       if (cookies > 0){
@@ -31,25 +38,35 @@ window.onload = function (){
         clearInterval(pusheenEats);
         if (myCookies > pusheenCookies){
           result.innerHTML = 'You\'ve Ate All The Cookies! Ya Fatteh!';
+          isActive = false;
         }else {
           result.innerHTML = 'Boo! Pusheen beat you in cookie eating!!';
+          isActive = false;
         }
       }
       cookieCount.innerHTML = cookies;
       pusheenCount.innerHTML = pusheenCookies;
+
+      document.getElementById('getcookie').onclick =
+        function getCookie (){
+          if (cookies > 0){
+            cookies -= 1;
+            myCookies += 1;
+          }
+          cookieCount.innerHTML = cookies;
+          myCount.innerHTML = myCookies;
+        };
     }, 100);
 
-    document.getElementById('getcookie').onclick =
-      function getCookie (){
-        if (cookies > 0){
-          cookies -= 1;
-          myCookies += 1;
-        }
-        cookieCount.innerHTML = cookies;
-        myCount.innerHTML = myCookies;
-      };
-  };
+  }
 
-  document.getElementById('start').onclick = startGame;
+  // document.getElementById('start').onclick = startGame;
+  document.getElementById('start').onclick = function (){
+    if (!isActive){
+      isActive = true;
+      startGame();
+    }
+    return null;
+  };
 
 };
