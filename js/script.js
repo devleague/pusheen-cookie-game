5569d9cb8dc1b3b861590c6e4b3227cc4310762a
Sean Nakamura <seanenakamura@gmail.com>
2015-08-07 17:26:17 -1000

    game will reset on clicking start

--- js/script.js
+++ js/script.js
@@ -9,40 +9,47 @@ window.onload = function (){
   var cookieCount = document.getElementById('cookies');
   var pusheenCount = document.getElementById('pusheencookies');
   var myCount = document.getElementById('mycookies');
+  var result = document.getElementById('result');
 
   cookieCount.innerHTML = cookies;
   myCount.innerHTML = myCookies;
   pusheenCount.innerHTML = pusheenCookies;
 
-  document.getElementById('start').onclick =
-    function startGame (){
-      var pusheenEats = window.setInterval(function (){
-        if (cookies > 0){
-          if (pusheenCookies < 100){
-            pusheenCookies += 1;
-            cookies -= 1;
-          }
+  var startGame = function (){
+    cookies = 100;
+    myCookies = 0;
+    pusheenCookies = 0;
+
+    var pusheenEats = window.setInterval(function (){
+      if (cookies > 0){
+        if (pusheenCookies < 100){
+          pusheenCookies += 1;
+          cookies -= 1;
         }
-        if (cookies === 0){
-          clearInterval(pusheenEats);
-          if (myCookies > pusheenCookies){
-            alert('You\'ve won!');
-          }else {
-            alert('YOU LOST ALL THE COOKIES!');
-          }
+      }
+      if (cookies === 0){
+        clearInterval(pusheenEats);
+        if (myCookies > pusheenCookies){
+          result.innerHTML = 'You\'ve Ate All The Cookies! Ya Fatteh!';
+        }else {
+          result.innerHTML = 'Boo! Pusheen beat you in cookie eating!!';
+        }
+      }
+      cookieCount.innerHTML = cookies;
+      pusheenCount.innerHTML = pusheenCookies;
+    }, 100);
+
+    document.getElementById('getcookie').onclick =
+      function getCookie (){
+        if (cookies > 0){
+          cookies -= 1;
+          myCookies += 1;
         }
         cookieCount.innerHTML = cookies;
-        pusheenCount.innerHTML = pusheenCookies;
-      }, 250);
+        myCount.innerHTML = myCookies;
+      };
+  };
 
-      document.getElementById('getcookie').onclick =
-        function getCookie (){
-          if (cookies > 0){
-            cookies -= 1;
-            myCookies += 1;
-          }
-          cookieCount.innerHTML = cookies;
-          myCount.innerHTML = myCookies;
-        };
-    };
+  document.getElementById('start').onclick = startGame;
+
 };
